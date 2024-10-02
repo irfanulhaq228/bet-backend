7cc5e69ce07d9b76d59433158ab4aff998a070c7
irfanulhaq228 <irfanulhaq228@gmail.com>
2024-10-02 22:58:55 +0500

    third commit

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const UserRouter = require("./Routes/userRoutes.js");
 
 dotenv.config();
 const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || '*',
+    optionsSuccessStatus: 200
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
