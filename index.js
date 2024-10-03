24f4f7e9815412734dfd5b970a36f3bb68bbf65d
irfanulhaq228 <irfanulhaq228@gmail.com>
2024-10-03 17:59:07 +0500

    index.js updated

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./db/db.js");
 
-const UserRouter = require("./Routes/userRoutes.js");
+// const UserRouter = require("./Routes/userRoutes.js");
+const userModel = require("./Models/UserModel.js");
 
 dotenv.config();
 const app = express();
@@ -27,7 +28,19 @@ app.get("/test", (req, res) => {
     res.json({ message: "Testing Successful" });
 });
 
-app.use("/user", UserRouter);
+// app.use("/user", UserRouter);
+app.get("/user", async (req, res) => {
+    try {
+        const user = await userModel.find();
+        if (user.length === 0) {
+            return res.status(400).json({ message: "User Data is Empty" })
+        }
+        return res.status(200).json({ message: "Data Sent Successfully", data: user });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Server Error!" })
+    }
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server runs at port ${process.env.PORT}`);
